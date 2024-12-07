1337116e226d234145c37a8e11ff323de41d80a9
backm <yousseffmohamedd22@gmail.com>
2024-12-07 02:14:25 +0200

    File Structure Assets Packages Layout

---
+++ Data/data.ts
@@ -0,0 +1,138 @@
+export const BaseInfo = {
+    name: "Youssef Mohamed Saleh",
+    position: "Full Stack Web Developer (MERN)",
+    description:
+      "A passionate and detail-oriented Full Stack Web Developer specializing in the MERN stack. With expertise in building scalable, dynamic, and user-centric web applications, I thrive on turning complex challenges into innovative solutions. Dedicated to delivering high-quality code, seamless user experiences, and robust back-end systems.",
+    profilePic: "/images/YoussefPhoto.jpg",
+  };
+  
+  
+  export const aboutInfo = {
+    title: "v",
+    description:
+      "Dynamic and detail-oriented MERN Stack Developer with 1 year of experience in designing, developing, and deploying scalable web applications. Proficient in crafting interactive user interfaces using React.js and building robust backends with Node.js and Express.js. I am skilled in database design and management with MongoDB and am adept at implementing RESTful APIs solutions. Demonstrated ability to collaborate in Agile teams, write clean and reusable code, and optimize applications for performance and scalability. Passionate about solving complex problems and continuously improving user experiences with modern web technologies.",
+    client: "10+",
+    experience: "1+",
+    project: "10+",
+    website: "5+",
+  };
+  
+  export const servicesData = [
+    {
+      id: 1,
+      title: " Web Applications",
+      description: "Tailored web apps built with the latest technologies.",
+      icon: "/images/s1.png",
+    },
+    {
+      id: 2,
+      title: "E-commerce Websites",
+      description:
+        "Secure online stores with user-friendly shopping and payments.",
+      icon: "/images/s2.png",
+    },
+    {
+      id: 3,
+      title: "API Integration",
+      description:
+        "Seamless integration for smooth data sharing and functionality.",
+      icon: "/images/s3.png",
+    },
+    {
+      id: 4,
+      title: "User Interfaces",
+      description:
+        "Engaging, responsive front-end designs for better user experiences.",
+      icon: "/images/s4.png",
+    },
+    {
+      id: 5,
+      title: "Database Solutions",
+      description:
+        "Efficient and scalable database management for your data needs.",
+      icon: "/images/s5.png",
+    },
+    {
+      id: 6,
+      title: "Real-Time Features",
+      description: "Add live chat, notifications, and real-time data updates.",
+      icon: "/images/s6.png",
+    },
+    {
+      id: 7,
+      title: "Speed Optimization",
+      description: "Boost performance with faster load times and optimization.",
+      icon: "/images/s7.png",
+    },
+    {
+      id: 8,
+      title: "Website Management",
+      description: "Easy content management systems with flexible controls.",
+      icon: "/images/s8.png",
+    },
+  ];
+  
+  export const projectData = [];
+  
+  export const skillsData = [
+    {
+      id: 1,
+      title: "React",
+      image: "/images/react.svg",
+      percent: "70%",
+    },
+    {
+      id: 2,
+      title: "CSS",
+      image: "/images/css.svg",
+      percent: "60%",
+    },
+    {
+      id: 3,
+      title: "JavaScript",
+      image: "/images/js.svg",
+      percent: "80%",
+    },
+    {
+      id: 4,
+      title: "TypeScript",
+      image: "/images/ts.svg",
+      percent: "60%",
+    },
+    {
+      id: 5,
+      title: "HTML",
+      image: "/images/html.svg",
+      percent: "80%",
+    },
+    {
+      id: 6,
+      title: "Node JS",
+      image: "/images/node.svg",
+      percent: "80%",
+    },
+    {
+      id: 7,
+      title: "MongoDB",
+      image: "/images/mongo.svg",
+      percent: "70%",
+    },
+    {
+      id: 8,
+      title: "Java",
+      image: "/images/python.svg",
+      percent: "70%",
+    },
+    {
+      id: 9,
+      title: "Firebase",
+      image: "/images/firebase.svg",
+      percent: "60%",
+    },
+  ];
+    
+  export const contactData = {
+    phone: "+20(1280041970)",
+    email: "yousseffmohamedd22@gmail.com",
+    address: "Cairo , Egypt",
+  };
